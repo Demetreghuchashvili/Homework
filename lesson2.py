537a5e2f277887803b0c032d69cc37aeaa27f25d
Demetreghuchashvili <143876987+Demetreghuchashvili@users.noreply.github.com>
2023-09-02 16:49:34 +0400

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,21 @@
+name= "demetre"
+
+surname= "ghuchashvili"
+
+
+
+age=11
+height=155.4
+know_proggraming=True
+
+print(name+ " " + surname)
+
+
+print(type(age))
+print(type(height))
+print(type(surname))
+print(type(name))
+print(type(know_proggraming))
+
+print(name+ " " +str(age))
+print("Hello, I am " + str(name) + " " + str(surname) + ". I am " + str(age) + " years old. My height is " + str(height)) 
